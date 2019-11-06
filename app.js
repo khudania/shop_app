13b76b920b046c03be238854a58199a25cb66334
Himanshu Khudania <hkhudania@gmail.com>
2019-11-06 17:13:33 +0530

    MongoDB Connection Done and created Collection Products

--- app.js
+++ app.js
@@ -4,36 +4,34 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database')
-
+const mongoConnect = require('./util/database').mongoConnect;
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-// const adminRoutes = require('./routes/admin');
-// const shopRoutes = require('./routes/shop');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  // User.findByPk(1)
+  // User.findById(1)
   //   .then(user => {
   //     req.user = user;
   //     next();
   //   })
   //   .catch(err => console.log(err));
+  next();
 });
 
-// app.use('/admin', adminRoutes);
-// app.use(shopRoutes);
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect((client) =>{
-  console.log(client)
-  app.listen(3000)
-})
-
+mongoConnect(() => {
+  app.listen(3000);
+});
