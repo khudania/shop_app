9cf7bb215c07589d83dd05f53d252664a9bf9357
Himanshu Khudania <hkhudania@gmail.com>
2019-12-24 11:12:29 +0530

    Update Products Finished

--- models/product.js
+++ models/product.js
@@ -1,19 +1,27 @@
-const mongodb = require('mongodb')
+const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 class Product {
-  constructor(title, price, description, imageUrl) {
+  constructor(title, price, description, imageUrl, id) {
     this.title = title;
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
+    this._id = id;
   }
 
   save() {
     const db = getDb();
-    return db
-      .collection('products')
-      .insertOne(this)
+    let dbOp;
+    if (this._id) {
+      // Update the product
+      dbOp = db
+        .collection('products')
+        .updateOne({ _id: new mongodb.ObjectId(this._id) }, { $set: this });
+    } else {
+      dbOp = db.collection('products').insertOne(this);
+    }
+    return dbOp
       .then(result => {
         console.log(result);
       })
@@ -38,18 +46,18 @@ class Product {
   }
 
   static findById(prodId) {
-    const db = getDb()
+    const db = getDb();
     return db
       .collection('products')
-      .find({ _id: mongodb.ObjectId(prodId) })
+      .find({ _id: new mongodb.ObjectId(prodId) })
       .next()
       .then(product => {
-        console.log(product)
-        return product
+        console.log(product);
+        return product;
       })
-      .catch(errr => {
-        console.log(err)
-      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 }
 
